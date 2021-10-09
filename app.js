14bee50d07eb3f1ea183a695f88d89d6ba8619dc
fishabo <jillisbeauity@gmail.com>
2021-10-09 23:42:46 +0800

    fix some bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 // set port
-const port = process.env.port || '8080'
+const port = process.env.PORT|| '8080'
 
 // set app
 const app = express()
